70a82610802ac1ae3d6c72ecc21bd7752fa152ad
Scott Leberknight <scottleber@gmail.com>
2020-10-30 19:56:18 -0400

    Add verification that the health check was NOT registered

--- src/test/java/org/kiwiproject/dropwizard/error/ErrorContextBuilderTest.java
+++ src/test/java/org/kiwiproject/dropwizard/error/ErrorContextBuilderTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
@@ -177,6 +178,10 @@ class ErrorContextBuilderTest {
 
         private void assertNoHealthCheck(SoftAssertions softly, ErrorContext errorContext) {
             softly.assertThat(errorContext.recentErrorsHealthCheck()).isEmpty();
+
+            var healthChecks = environment.healthChecks();
+            verify(healthChecks, never())
+                    .register(eq("recentErrorsHealthCheck"), isA(RecentErrorsHealthCheck.class));
         }
 
         @Nested
